24f67cdec27467ccc45fa5ad3141454a81f4dae1
Lorenzo <lmolina@gaming.tech>
2022-12-07 14:57:30 +0100

    rtjhrnhj

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props){
+  return <Router>
+      <Switch>
+        <Route exact path="/"> {/*ici on met l'URL dans le navigateur*/}
+          <Home /> {/*ici on donne la page à afficher en fonction de cette URL*/}
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+      </Switch>
+  </Router>
 }
 
 export default App;
